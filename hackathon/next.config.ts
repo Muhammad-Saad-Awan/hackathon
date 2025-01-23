80955fb9224010b61c394f004e361931cb677592
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2025-01-24 00:04:23 +0500

    Sanity Added

--- hackathon/next.config.ts
+++ hackathon/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cdn.sanity.io'], // Add the hostname here
+  },
 };
 
 export default nextConfig;
---
+++ hackathon/sanity/schemaTypes/product.ts
@@ -0,0 +1,69 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'id',
+        title: 'ID',
+        type: 'string',
+        description: 'Unique identifier for the product',
+      },
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true, // Enable image cropping
+        },
+      },
+      {
+        name: 'imagePath',
+        title: 'Image Path',
+        type: 'url',
+        description: 'Provide an image URL if no image is uploaded',
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name: 'discountPercentage',
+        title: 'Discount Percentage',
+        type: 'number',
+      },
+      {
+        name: 'isFeaturedProduct',
+        title: 'Is Featured Product',
+        type: 'boolean',
+      },
+      {
+        name: 'stockLevel',
+        title: 'Stock Level',
+        type: 'number',
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+      },
+    ],
+    preview: {
+      select: {
+        title: 'name',
+        media: 'image',
+      },
+    },
+  };
+  
